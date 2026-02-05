fd63b67c85d963e6f3cffc180b4aac0a039708a5
progRizvi <prog.rizvi@gmail.com>
2026-02-05 23:04:30 +0600

    update the menu item

--- Modules/Menu/resources/views/admin/items/index.blade.php
+++ Modules/Menu/resources/views/admin/items/index.blade.php
@@ -136,12 +136,6 @@
                                 </td>
                                 <td>
                                     {{ $item->name }}
-                                    @if ($item->is_new)
-                                        <span class="badge bg-info" title="New">{{ __('New') }}</span>
-                                    @endif
-                                    @if ($item->is_popular)
-                                        <span class="badge bg-warning text-dark" title="Popular">{{ __('Popular') }}</span>
-                                    @endif
                                 </td>
                                 <td>{{ $item->category->name ?? 'N/A' }}</td>
                                 <td>{{ currency($item->base_price) }}</td>
